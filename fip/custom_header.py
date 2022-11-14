4b4df7dfe313d3e594419ebe928b6f3f91e4a1c3
Manuel <manuel.trageser789@googlemail.com>
2022-11-14 15:51:23 +0100

    added custom headers that are used for preprocessing data

---
+++ fip/custom_header.py
@@ -0,0 +1,100 @@
+from scapy.all import DNSQRField, DNSRRField, Packet, BitField, BitEnumField, FlagsField, XByteField, ByteField, ByteEnumField, StrField, IP_PROTOS, TCPOptionsField
+from scapy.layers.inet6 import  ipv6nh
+
+class custom_IP(Packet):
+    name = "IP"
+    fields_desc = [
+        BitField("version", 4, 4),
+        FlagsField("flags", 0, 4, ["R", "DF", "MF"]), # normally 3 bits last bit will always be 0
+        XByteField("tos", 0),
+        ByteField("ttl", 64),
+        ByteEnumField("proto", 0, IP_PROTOS),
+        ]
+
+class custom_IPv6(Packet):
+    name = "IPv6"
+    field_desc = [
+        BitField("version", 6, 8), # normally 4 bits, last 4bits will always be 0
+        BitField("tc", 0, 8),
+        ByteEnumField("nh", 59, ipv6nh),
+        ByteField("hlim", 64)
+    ]
+
+class custom_TCP(Packet):
+    name = "TCP"
+    fields_desc = [
+        FlagsField("flags", 0x2, 16, "FSRPAUECN"),
+        TCPOptionsField("options", "")
+    ]
+
+class custom_UDP(Packet):
+    name = "UDP"
+    fields_desc = []
+
+class custom_HTTP(Packet):
+    def self_build(self):
+        p = b""
+
+        for f in self.fields_desc:
+            val = self.getfieldval(f.name)
+            #when Value is not set
+            if not val:
+                continue
+            if f.name not in ['Method', 'Path', 'Status_Code']:
+                val = bytes((f.name).encode()) + b": " + bytes(val)
+            if f.name in ['Method', 'Path', 'Status_Code']:
+                seperator = b' '
+            else:
+                seperator = b'\r\n'
+
+            p = f.addfield(self, p, val + seperator)
+        
+        return p
+
+
+class custom_HTTP_Request(custom_HTTP):
+    name = "HTTP Response"
+    fields_desc = [
+        StrField("Method", "GET"),
+        StrField("Path", "/"),
+        StrField("User_Agent", None),
+        StrField("Content_Type", None),
+        StrField("Connection", None),
+        StrField("Accept", None),
+        StrField("Accept_Charset", None),
+        StrField("Accept_Encoding", None),
+        StrField("Cookie", None),
+        StrField("TE", None)
+    ]
+
+class custom_HTTP_Response(custom_HTTP):
+    name = "HTTP Response"
+    fields_desc = [
+        StrField("Status_Code", "200"),
+        StrField("Connection", None),
+        StrField("Content_Encoding", None),
+        StrField("Content_Type", None),
+        StrField("Server", None),
+        StrField("Set_Cookie", None)
+    ]
+
+class custom_DNS(Packet):
+    name = "DNS"
+    fields_desc = [
+        BitField("qr", 0, 1),
+        BitEnumField("opcode", 0, 4, {0: "QUERY", 1: "IQUERY", 2: "STATUS"}),
+        BitField("aa", 0, 1),
+        BitField("tc", 0, 1),
+        BitField("rd", 1, 1),
+        BitField("ra", 0, 1),
+        BitField("z", 0, 1),
+        BitField("ad", 0, 1),
+        BitField("cd", 0, 1),
+        BitEnumField("rcode", 0, 4, {0: "ok", 1: "format-error",
+                                     2: "server-failure", 3: "name-error",
+                                     4: "not-implemented", 5: "refused"}),
+        DNSQRField("qd", "", None),
+        DNSRRField("an", "", None),
+        DNSRRField("ns", "", None),
+        DNSRRField("ar", "", None),
+    ]
--- fip/packets.py
+++ fip/packets.py
@@ -1,12 +1,16 @@
 from abc import ABC, abstractmethod
 
-from scapy.all import IP, IPv6, Ether, Packet, wrpcap, rdpcap, RandIP, RandMAC
+from scapy.all import DNS, IP, UDP, IPv6, Ether, Packet, wrpcap, rdpcap, RandIP, RandMAC, TCP
+from typing import Type
+from scapy.layers.http import HTTPRequest, HTTPResponse
 
 import os
 
 from fip.ssh import SSH
+from fip.custom_header import custom_IP, custom_IPv6, custom_HTTP_Request, custom_HTTP_Response, custom_DNS, custom_TCP, custom_UDP
 
-class PacketProcessed(ABC):
+
+class PCAPProcessed(ABC):
     def __init__(self, file: str, packets: list[Packet]) -> None:
         self.file = file
         self.packets = packets
@@ -37,13 +41,13 @@ class PacketProcessor(ABC):
     def __iter__(self):
         return self
 
-    def next(self) -> PacketProcessed:
+    def next(self) -> PCAPProcessed:
         """
         implement the iterator protocol on a set of packets in a pcap file
         """
         try:
             file = self.files.pop(0)
-            return PacketProcessed(os.path.basename(file), self.read_packet(file))
+            return PCAPProcessed(os.path.basename(file), self.read_packets(file))
         except IndexError:
             raise StopIteration
 
@@ -52,7 +56,7 @@ class PacketProcessor(ABC):
     def write_packet(self) -> None:
         wrpcap(f'{self.filename}_converted.pcap', self.packets, append=True)
 
-    def read_packet(self, file) -> str:
+    def read_packets(self, file) -> str:
         pcap = rdpcap(filename=file)
         packets = []
         for pkt in pcap:
@@ -62,18 +66,89 @@ class PacketProcessor(ABC):
         return packets
 
     def preprocessing(self, packet: Packet) -> Packet:
-        processed_packet = packet
-        if processed_packet.haslayer(Ether):
-            processed_packet[Ether].src = RandMAC()._fix()
-            processed_packet[Ether].dst = RandMAC()._fix()
+        for layer_class in packet.getlayer():
+            packet = self.preprocess_layer(packet, layer_class)
+    
+    def preprocess_layer(self, packet: Packet, layer_class: Type[Packet]) -> Packet:
+        if layer_class == IP:
+            new_layer = custom_IP(version=packet[IP].version,
+                                        tos=packet[IP].tos, ttl=packet[IP].ttl,
+                                        flags=packet[IP].flags,
+                                        proto=packet[IP].proto)
+        elif layer_class == IPv6:
+            new_layer = custom_IPv6(
+                version = packet[IPv6].version,
+                tc = packet[IPv6].tc, 
+                nh = packet[IPv6].nh,
+                hlim = packet[IPv6].hlim,
+            )
+        
+        elif layer_class == TCP:
+            new_layer = custom_TCP(flags=packet[TCP].flags, 
+            options=packet[TCP].options)
 
-        if processed_packet.haslayer(IP):
-            processed_packet[IP].src = RandIP()._fix()
-            processed_packet[IP].dst =RandIP()._fix()
-        elif processed_packet.haslayer(IPv6):
-            processed_packet[IPv6].src = RandIP()._fix()
-            processed_packet[IPv6].dst = RandIP()._fix()
-        return processed_packet
+        elif layer_class == UDP:
+            new_layer = custom_UDP()
+
+        elif layer_class == HTTPRequest:
+            new_layer = custom_HTTP_Request(
+                Method = packet[HTTPRequest].Method,
+                Path = packet[HTTPRequest].Path,
+                User_Agent = packet[HTTPRequest].User_Agent,
+                Content_Type = packet[HTTPRequest].Content_Type,
+                Connection = packet[HTTPRequest].Connection,
+                Accept = packet[HTTPRequest].Accept,
+                Accept_Charset = packet[HTTPRequest].Accept_Charset,
+                Cookie = packet[HTTPRequest].Cookie,
+                TE = packet[HTTPRequest].TE
+                )
+
+        elif layer_class == HTTPResponse:
+            new_layer = custom_HTTP_Response(
+                Status_Code = packet[HTTPResponse].Status_Code,
+                Server = packet[HTTPResponse].Server,
+                Content_Type = packet[HTTPResponse].Content_Type,
+                Connection = packet[HTTPResponse].Connection,
+                Content_Encoding = packet[HTTPResponse].Content_Encoding,
+                Set_Cookie = packet[HTTPResponse].Set_Cookie,
+                )
+        elif layer_class == DNS:
+            new_layer = custom_DNS(
+                qr = packet[DNS].qr,
+                opcode = packet[DNS].opcode,
+                aa = packet[DNS].aa,
+                tc = packet[DNS].tc,
+                rd = packet[DNS].rd,
+                ra = packet[DNS].ra,
+                z = packet[DNS].z,
+                ad = packet[DNS].ad,
+                cd = packet[DNS].cd,
+                rcode = packet[DNS].rcode,
+                qd = packet[DNS].qd,
+                an = packet[DNS].an,
+                ns = packet[DNS].ns,
+                ar = packet[DNS].ar
+            )
+        
+        else:
+            return packet
+
+        layers = packet.layers()
+        for i, layer in enumerate(layers):
+            if layer == layer_class:
+                if i != (len(layers) - 1):
+                    after_layer = packet[layers[i+1]]
+                    new_layer /= after_layer
+                if i != 0:
+                    packet[layers[i-1]].remove_payload()
+                    packet /= new_layer
+                else:
+                    packet = new_layer
+                break
+
+        return packet
+
+
 
     def __exit__(self, exc_type, exc_value, tracback) -> None:
         pass
