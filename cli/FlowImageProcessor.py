438644c358767189b20b300701c47bc5441c9d91
Manuel <manuel.trageser789@googlemail.com>
2022-11-16 11:51:18 +0100

    added min packets per flow and min FlowImage dimension

--- cli/FlowImageProcessor.py
+++ cli/FlowImageProcessor.py
@@ -33,7 +33,23 @@ def cli():
     type=click.STRING,
     default="none",
     help="Applies a preprocessing to the input data:\n none: No preprocessing\n payload: Only payload data is used\n header: Preprocesses headers (DNS,HTTP,IP,IPv6,TCP,UDP supported) to remove some biasing data")
+@click.option(
+    "-mid",
+    "--min_im_dim",
+    "min_image_dim",
+    type=int,
+    default =  0,
+    help="Minimum dim ouput images need to have, 0=No minimum dim"
+)
+@click.option(
+    "-mp",
+    "--min_packets",
+    "min_packets_per_flow",
+    type=int,
+    default =  0,
+    help="Minimum packets that a FlowImage needs to have, 0=No minimum packets per flow"
+)
 @cli.command(name="extract")
-def extract(input_dir, output_dir, preprocessing_type):
+def extract(input_dir, output_dir, preprocessing_type, min_image_dim, min_packets_per_flow):
     runner = Runner(7)
-    runner.run(input_dir, output_dir, preprocessing_type, width=128, append=False, tiled=True)
+    runner.run(input_dir, output_dir, preprocessing_type, min_image_dim, min_packets_per_flow, width=128, append=False, tiled=True)
