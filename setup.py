96ee5d378539b179269afd1b2e8424b4cf6b369e
Stefan Machmeier <stefan-machmeier@outlook.com>
2023-03-23 17:02:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open("fip/version.py") as f:
     exec(f.read())  # set __version__
 
 setup(
-    name="Flow Image Processor",
+    name="heiFIP",
     version=__version__,
     description="A useful module to create packet flows into images",
     author="Stefan Machmeier",
     python_requires=">=3.7",
     author_email="stefan.machmeier@uni-heidelberg.de",
     maintainer="Stefan Machmeier",
-    url="https://gitlab.urz.uni-heidelberg.de/it-sec/schwachstellenmanagement",
+    url="https://github.com/stefanDeveloper/heiFIP",
     long_description=README,
     include_package_data=True,
     packages=find_packages(),
@@ -32,6 +32,24 @@ setup(
         "numpy==1.23.1",
         "click==8.1.3",
         "tqdm==4.64.1"
+    ],
+    platforms=["Linux", "Mac OS-X", "Windows", "Unix"],
+    classifiers=[
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Security',
+        'Topic :: Internet :: Log Analysis',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
     ],
     extras_require={
         "cli": ["click==8.0.3", "click-help-colors==0.9.1"],
