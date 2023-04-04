a1de26cfae94f221724ddc0ecf8717003f4c88b8
Stefan Machmeier <stefan-machmeier@outlook.com>
2023-04-04 14:29:29 +0200

    bumpversion

--- heifip/exceptions/__init__.py
+++ heifip/exceptions/__init__.py
@@ -1,2 +1,12 @@
+__author__ = "Stefan Machmeier"
+__copyright__ = "Copyright 2023, heiFIP"
+__credits__ = ["Manuel Trageser"]
+__license__ = "EUPL"
+__version__ = "1.0.0"
+__maintainer__ = "Stefan Machmeier"
+__email__ = "stefan.machmeier@uni-heidelberg.de"
+__status__ = "Production"
+
 class FIPWrongParameterException(Exception):
     """Wrong Parameter passed"""
+
