fd8dbe95d6ecbdeb96028ec08409223a8f67ec5a
Stefan Machmeier <stefan.machmeier@outlook.com>
2023-04-11 19:06:57 +0200

    Update example

--- example.py
+++ example.py
@@ -1,31 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from heifip.extractor import FIPExtractor
 from heifip.images.flow import FlowImage
+from heifip.images.flow_tiled_auto import FlowImageTiledAuto
+from heifip.images.flow_tiled_fixed import FlowImageTiledFixed
 from heifip.images.markovchain import (MarkovTransitionMatrixFlow,
                                        MarkovTransitionMatrixPacket)
 from heifip.images.packet import PacketImage
 from heifip.layers import PacketProcessorType
 
-import numpy as np
-import matplotlib.pyplot as plt
-
 extractor = FIPExtractor()
 imgs = extractor.create_image_from_file(
-    "./tests/pcaps/http/bro.org.pcap",
+    "/home/smachmeier/data/USTC-TFC2016-master/2_Session/AllLayers/FTP-ALL/FTP.pcap.TCP_1-1-221-163_51670_1-2-197-24_21.pcap",
     PacketProcessorType.NONE,
     MarkovTransitionMatrixFlow,
-    0,
-    0,
-    0,
-    16,
-    # True,
-    # 8,
-    # 0,
-    # 128,
-    # True,
-    # True,
-    # True
+    0, # min_image_dim
+    0, # max_image_dim
+    3, # min_packets
+    0, # max_packets
+    True, # remove_duplicates,
+    8
+    # 30, # dim
+    # 0, # fill
+    # True # auto_dim
 )
-# extractor.save_image(img[0], "./test2.jpg")
+i = 0
+for img in imgs:
+    extractor.save_image(img, f"/home/smachmeier/Documents/projects/heiFIP/data/benign/{i}.png")
+    i += 1
+
 # fig = plt.figure(figsize=(16, 16))
 # columns = 4
 # rows = 4
@@ -36,5 +40,5 @@ imgs = extractor.create_image_from_file(
 #     plt.imshow(imgs[i])
 # plt.savefig('test.pdf', dpi=fig.dpi)
 
-plt.imshow(imgs[0])
-plt.savefig('test.pdf')
+# plt.imshow(imgs[0])
+# plt.savefig('test.pdf')
